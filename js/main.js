a7a0a2fa94dc0b7423c1e3e4bdb322f19d670710
cessar <contatosimwebsite@gmail.com>
2024-10-02 21:47:13 -0400

    1º Commit

--- js/main.js
+++ js/main.js
@@ -1,42 +1,77 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true
+    });
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+    $('.menu-hamburger').click(function () {
+        $('nav').slideToggle();
+    })
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+    $('.date').mask('00/00/0000');
+    $('.time').mask('00:00:00');
+    $('.date_time').mask('00/00/0000 00:00:00');
+    $('.cep').mask('00000-000');
+    $('.phone').mask('0000-0000');
+    $('#campo-telefone').mask('(00) 0000-0000');
+    $('.phone_us').mask('(000) 000-0000');
+    $('.mixed').mask('AAA 000-S0S');
+    $('.cpf').mask('000.000.000-00', { reverse: true });
+    $('.cnpj').mask('00.000.000/0000-00', { reverse: true });
+    $('.money').mask('000.000.000.000.000,00', { reverse: true });
+    $('.money2').mask("#.##0,00", { reverse: true });
+    $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+        translation: {
+            'Z': {
+                pattern: /[0-9]/, optional: true
+            }
+        }
+    });
+    $('.ip_address').mask('099.099.099.099');
+    $('.percent').mask('##0,00%', { reverse: true });
+    $('.clear-if-not-match').mask("00/00/0000", { clearIfNotMatch: true });
+    $('.placeholder').mask("00/00/0000", { placeholder: "__/__/____" });
+    $('.fallback').mask("00r00r0000", {
+        translation: {
+            'r': {
+                pattern: /[\/]/,
+                fallback: '/'
+            },
+            placeholder: "__/__/____"
+        }
+    });
+    $('.selectonfocus').mask("00/00/0000", { selectOnFocus: true })
 
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
+    $('form').validate({
+        rules:{
+            nome:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            telefone:{
+                required: true
+            },
         },
-        email: {
-            requerido: true,
-            email: true
+        messages:{
+            nome:'Informe o nome completo'
         },
-        telefone: {
-            requerido: true
+        submitHandler: function(form) {
+           console.log(form)
         },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
+        invalidHandler: function(evento, validador){
+            let camposIncrretos = validador.numberOfInvalids();
+            console.log(camposIncrretos)
+        }
+    })
+
+    $('.lista-veiculos button').click( function(){
+        const destino =$('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+        $('#veiculo-interesse').val(nomeVeiculo)
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
 })
